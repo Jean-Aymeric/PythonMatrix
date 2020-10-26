77ad3fb9edb4802b52e55404d0ae15e3478d9c26
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-26 11:49:36 +0100

    fix step7

--- PythonMatrix/PythonMatrix.py
+++ PythonMatrix/PythonMatrix.py
@@ -3,14 +3,14 @@ import time
 
 digitList = []
 for i in range (0, 100) :
-    digitList.append (random.randint(31,255))
+    digitList.append (random.randint(32,255))
 
 atLeastOne = True
 while atLeastOne :
     atLeastOne = False
     for i in range (len (digitList)) :
         print (chr(digitList[i]), end="")
-        if (digitList[i] != 32) :
+        if (digitList[i] >= 32) :
             digitList[i] -= 1
             atLeastOne = True
     print ()

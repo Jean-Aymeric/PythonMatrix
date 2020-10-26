3be496e2caacb7169505e4e7661d940c96c78c7e
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-26 12:39:32 +0100

    step10 done

--- PythonMatrix/PythonMatrix.py
+++ PythonMatrix/PythonMatrix.py
@@ -4,17 +4,23 @@ import os
 
 os.system("color 02")
 digitList = []
+max = 32
 for i in range (0, 100) :
     digitList.append (random.randint(32,255))
+    if max < digitList[i] :
+        max = digitList[i]
 
 atLeastOne = True
 while atLeastOne :
     atLeastOne = False
     for i in range (len (digitList)) :
-        print (chr(digitList[i]), end="")
-        if (digitList[i] > 32) :
-            digitList[i] -= 1
+        if (255-digitList[i] < max):
+            print(" ", end="")
             atLeastOne = True
-
+        else :
+            print (chr(255-digitList[i]+32), end="")
+            if (digitList[i] > 32) :
+                digitList[i] -= 1
+    max -= 1
     print ()
     time.sleep(0.02)

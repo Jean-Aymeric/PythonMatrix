4c326645f69f2fcadf06bc02acc0a3b9d533056c
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-26 09:25:13 +0100

    step2 done

--- PythonMatrix/PythonMatrix.py
+++ PythonMatrix/PythonMatrix.py
@@ -1,5 +1,15 @@
-number = int(input ("Entrez un nombre : "))
+digitList = []
+for i in range (0, 10) :
+    digitList.append (i)
 
-for i in range (number, -1, -1) :
-    print (i)
-print (" ")
+atLeastOne = True
+while atLeastOne :
+    atLeastOne = False
+    for i in range (len (digitList)) :
+        if (digitList[i] == -1) :
+            print (" ", end="")
+        else :
+            print (digitList[i], end="")
+            digitList[i] -= 1
+            atLeastOne = True
+    print ()

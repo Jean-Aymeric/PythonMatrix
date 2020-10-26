9b09e3e21d173b341f76f48a54f59df4614ff07e
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-26 11:45:01 +0100

    step7 done

--- PythonMatrix/PythonMatrix.py
+++ PythonMatrix/PythonMatrix.py
@@ -2,16 +2,14 @@ import random
 
 digitList = []
 for i in range (0, 100) :
-    digitList.append (random.randint(0,9))
+    digitList.append (random.randint(31,255))
 
 atLeastOne = True
 while atLeastOne :
     atLeastOne = False
     for i in range (len (digitList)) :
-        print (digitList[i], end="")
-        if (digitList[i] == 0 or digitList[i] == " ") :
-            digitList[i] = " "
-        else :
+        print (chr(digitList[i]), end="")
+        if (digitList[i] != 32) :
             digitList[i] -= 1
             atLeastOne = True
     print ()

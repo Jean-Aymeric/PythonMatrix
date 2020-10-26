f2c4ed0d6bcb9d0d9f4c5e4009751c7f530b9987
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-26 10:36:22 +0100

    step 5 done

--- PythonMatrix/PythonMatrix.py
+++ PythonMatrix/PythonMatrix.py
@@ -2,11 +2,14 @@ digitList = []
 for i in range (0, 10) :
     digitList.append (i)
 
-for j in range(10):
+atLeastOne = True
+while atLeastOne :
+    atLeastOne = False
     for i in range (len (digitList)) :
         print (digitList[i], end="")
         if (digitList[i] == 0 or digitList[i] == " ") :
             digitList[i] = " "
         else :
             digitList[i] -= 1
+            atLeastOne = True
     print ()

e5e337a07f7de153d852ea81dd2e143978c83a4d
Jean-Aymeric Diet <jeanaymeric@gmail.com>
2020-10-26 09:42:54 +0100

    step3 done

--- PythonMatrix/PythonMatrix.py
+++ PythonMatrix/PythonMatrix.py
@@ -1,4 +1,12 @@
 digitList = []
 for i in range (0, 10) :
     digitList.append (i)
-print (digitList)
+
+for i in range (len (digitList)) :
+    if (digitList[i] == -1) :
+        print (" ", end="")
+    else :
+        print (digitList[i], end="")
+        digitList[i] -= 1
+print ()
+print(digitList)
